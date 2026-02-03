60bd87ae6ce734a3cf1d72d18fc96a12b1e56f1e
Dazhi Wang <ch2ohch2oh@gmail.com>
2026-02-03 00:10:25 -0500

    feat: Implement right-click context menu with a quit option for the status bar button and update its font to monospaced digits.

--- Sources/SysMonitor/StatusBarController.swift
+++ Sources/SysMonitor/StatusBarController.swift
@@ -17,20 +17,33 @@ class StatusBarController {
         
         if let button = statusItem.button {
             button.title = "Initializing..."
-            button.font = NSFont.monospacedSystemFont(ofSize: 12, weight: .regular)
+            button.font = NSFont.monospacedDigitSystemFont(ofSize: 12, weight: .regular)
             button.action = #selector(togglePopover(_:))
             button.target = self
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
         
         startTimer()
     }
     
     @objc func togglePopover(_ sender: AnyObject?) {
-        if let button = statusItem.button {
-            if popover.isShown {
-                popover.performClose(sender)
-            } else {
-                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        let event = NSApp.currentEvent
+        
+        if event?.type == .rightMouseUp {
+            let menu = NSMenu()
+            menu.addItem(NSMenuItem(title: "Quit SysMonitor", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+            
+            // Pop up the menu at the cursor location or properly anchored to button
+            if let button = statusItem.button {
+                 menu.popUp(positioning: nil, at: NSPoint(x: 0, y: button.bounds.height), in: button)
+            }
+        } else {
+            if let button = statusItem.button {
+                if popover.isShown {
+                    popover.performClose(sender)
+                } else {
+                    popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+                }
             }
         }
     }
